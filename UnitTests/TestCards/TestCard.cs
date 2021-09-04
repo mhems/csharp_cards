230fcd63de96f656cc1eec168b8d1f3bc1e1eba1
matthew <matthems@gmail.com>
2021-09-04 14:21:53 -0400

    update namespace name

--- UnitTests/TestCards/TestCard.cs
+++ UnitTests/TestCards/TestCard.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace CardsTests
+namespace TestCards
 {
     [TestClass]
     public class TestCard
--- UnitTests/TestCards/TestHand.cs
+++ UnitTests/TestCards/TestHand.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Cards;
 
-namespace CardsTests
+namespace TestCards
 {
     [TestClass]
     public class TestHand
