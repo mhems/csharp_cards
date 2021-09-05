556a6da1ee82766c8b5b5630a932e6e1f75bb66a
matthew <matthems@gmail.com>
2021-09-05 15:14:43 -0400

    add Player/Dealer classes

---
+++ Cards/Player.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards
+{
+    public class Player
+    { 
+        public Bank Bank { get; }
+        public string Name { get; protected set; }
+        public int Id { get; protected set; }
+       
+        private static int id = 0;
+
+        public Player(string name) : this(name, new Bank()) { }
+
+        protected Player(string name, Bank bank)
+        {
+            Name = name;
+            this.Bank = bank;
+            Id = id;
+            id++;
+        }
+
+        public void Bet(int amount)
+        {
+            Bank.Withdraw(amount);
+        }
+
+        public void Payout(int amount)
+        {
+            Bank.Deposit(amount);
+        }
+
+        public override string ToString()
+        {
+            return $"Player '{Name}' ({Id}) : ${Bank.Balance}";
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other is Player)
+            {
+                return GetHashCode() == other.GetHashCode();
+            }
+            return false;
+        }
+    }
+
+    public class Dealer : Player
+    {
+        public Dealer() : base("Dealer", new HouseBank()) { } 
+    }
+}
---
+++ UnitTests/TestCards/TestPlayer.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cards;
+
+namespace TestCards
+{
+    [TestClass]
+    public class TestPlayer
+    {
+        [TestMethod]
+        public void TestCtor()
+        {
+            Player p1 = new("Alice");
+            Player p2 = new("Bob");
+
+            Assert.AreEqual("Alice", p1.Name);
+            Assert.AreEqual(0, p1.Bank.Balance);
+
+
+            Assert.AreEqual("Bob", p2.Name);
+            Assert.AreEqual(0, p2.Bank.Balance);
+
+            Assert.AreNotEqual(p1.Id, p2.Id);
+            Assert.AreNotEqual(p1.GetHashCode(), p2.GetHashCode());
+            Assert.IsFalse(p1.Equals(p2));
+
+            Player p3 = new("Alice");
+            Assert.IsFalse(p1.Equals(p3));
+        }
+
+        [TestMethod]
+        public void TestPlayerBank()
+        {
+            Player p1 = new("Alice");
+
+            p1.Payout(100);
+            Assert.AreEqual(100, p1.Bank.Balance);
+
+            p1.Bet(50);
+            Assert.AreEqual(50, p1.Bank.Balance);
+        }
+
+        [TestMethod]
+        public void TestDealer()
+        {
+            Dealer dealer = new();
+            Assert.IsTrue(dealer.Bank is HouseBank);
+            Assert.IsTrue(dealer.ToString().Contains("Dealer"));
+        }
+    }
+}
