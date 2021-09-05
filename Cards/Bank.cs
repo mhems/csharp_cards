b1860c941cb27355d073424a8a756e3a6141cf7f
matthew <matthems@gmail.com>
2021-09-05 14:45:57 -0400

    add Bank class

---
+++ Cards/Bank.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards
+{
+    public class InsufficientFundsException : Exception
+    {
+        public InsufficientFundsException() : base() { }
+        public InsufficientFundsException(string msg) : base(msg) { }
+        public InsufficientFundsException(string msg, Exception inner) : base(msg, inner) { }
+    }
+
+    public class Bank
+    {
+        public EventHandler<BankTransactionEventArgs> Withdrawn;
+        public EventHandler<BankTransactionEventArgs> Deposited;
+
+        public int Balance { get; private set; } = 0;
+
+        public Bank(int balance = 0)
+        {
+            if (balance < 0)
+            {
+                throw new ArgumentException("Cannot construct a Bank with a negative balance");
+            }
+            Balance = balance;
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Cannot withdraw a negative amount");
+            }
+            if (amount > Balance)
+            {
+                throw new InsufficientFundsException($"Cannot withdraw ${amount} from ${Balance}");
+            }
+            Balance -= amount;
+            Withdrawn?.Invoke(this, new BankTransactionEventArgs(-amount, Balance));
+        }
+
+        public void Deposit(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Cannot deposit a negative amount");
+            }
+            Balance += amount;
+            Deposited?.Invoke(this, new BankTransactionEventArgs(amount, Balance));
+        }
+
+    }
+
+    public class BankTransactionEventArgs : EventArgs
+    {
+        public int Amount { get ; }
+        public int Balance { get ; }
+
+        public BankTransactionEventArgs(int Amount, int Balance)
+        {
+            this.Amount = Amount;
+            this.Balance = Balance;
+        }
+    }
+}
---
+++ UnitTests/TestCards/TestBank.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cards;
+
+namespace TestCards
+{
+    [TestClass]
+    public class TestBank
+    {
+        private bool gotDepositEvent;
+        private bool gotWithdrawEvent;
+        private int eventAmount;
+        private int eventBalance;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            gotDepositEvent = false;
+            gotWithdrawEvent = false;
+            eventAmount = 0;
+        }
+
+        private void DepositHandler(object obj, BankTransactionEventArgs args)
+        {
+            gotDepositEvent = true;
+            eventAmount = args.Amount;
+            eventBalance = args.Balance;
+        }
+
+        private void WithdrawHandler(object obj, BankTransactionEventArgs args)
+        {
+            gotWithdrawEvent = true;
+            eventAmount = args.Amount;
+            eventBalance = args.Balance;
+        }
+
+        [TestMethod]
+        public void TestDeposit()
+        {
+            Bank bank = new (100);
+            bank.Deposited += DepositHandler;
+            bank.Withdrawn += WithdrawHandler;
+
+            Assert.ThrowsException<ArgumentException>(() => bank.Deposit(-10));
+            Assert.IsFalse(gotDepositEvent);
+            Assert.IsFalse(gotWithdrawEvent);
+
+            bank.Deposit(50);
+
+            Assert.AreEqual(150, bank.Balance);
+            Assert.IsTrue(gotDepositEvent);
+            Assert.IsFalse(gotWithdrawEvent);
+            Assert.AreEqual(150, eventBalance);
+            Assert.AreEqual(50, eventAmount);
+        }
+
+        [TestMethod]
+        public void TestWithdraw()
+        {
+            Bank bank = new(100);
+            bank.Deposited += DepositHandler;
+            bank.Withdrawn += WithdrawHandler;
+
+            Assert.ThrowsException<ArgumentException>(() => bank.Withdraw(-10));
+            Assert.IsFalse(gotDepositEvent);
+            Assert.IsFalse(gotWithdrawEvent);
+
+            bank.Withdraw(100);
+
+            Assert.AreEqual(0, bank.Balance);
+            Assert.IsFalse(gotDepositEvent);
+            Assert.IsTrue(gotWithdrawEvent);
+            Assert.AreEqual(0, eventBalance);
+            Assert.AreEqual(-100, eventAmount);
+            gotWithdrawEvent = false;
+
+            Assert.ThrowsException<InsufficientFundsException>(() => bank.Withdraw(5));
+            Assert.IsFalse(gotDepositEvent);
+            Assert.IsFalse(gotWithdrawEvent);
+            Assert.AreEqual(0, bank.Balance);
+        }
+    }
+}
