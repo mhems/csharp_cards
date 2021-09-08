29dc2845e43f450830c41c66322e882f084bef45
matthew <matthems@gmail.com>
2021-09-08 19:40:32 -0400

    update count and add tests

--- Blackjack/Blackjack/BlackjackCount.cs
+++ Blackjack/Blackjack/BlackjackCount.cs
@@ -20,15 +20,15 @@ namespace Blackjack
 
     public class BlackjackCount
     {
-        public static readonly Dictionary<BlackjackCountEnum, List<float>> deltaMap = new()
+        private static readonly Dictionary<BlackjackCountEnum, List<float>> deltaMap = new()
         {
             { BlackjackCountEnum.HiLo, new List<float>() { 1, 1, 1, 1, 1, 0, 0, 0, -1, -1 } },
-            { BlackjackCountEnum.HiOptOne, new List<float>() { 0, 1, 1, 1, 1, 0, 0, 0, -1, -1, -1, -1, 0 } },
-            { BlackjackCountEnum.HiOptTwo, new List<float>() {1, 1, 2, 2, 1, 1, 0,  0, -2, -2, -2, -2,  0 } },
-            { BlackjackCountEnum.KO, new List<float>() {1, 1, 1, 1, 1, 1, 0,  0, -1, -1, -1, -1, -1 } },
-            { BlackjackCountEnum.OmegaTwo, new List<float>() {1, 1, 2, 2, 2, 1, 0, -1, -2, -2, -2, -2,  0 } },
-            { BlackjackCountEnum.RedSeven, new List<float>() {1, 1, 1, 1, 1, 0.5f, 0,  0, -1, -1, -1, -1, -1 } },
-            { BlackjackCountEnum.ZenCount, new List<float>() {1, 1, 2, 2, 2, 1, 0,  0, -2, -2, -2, -2, -1 } }
+            { BlackjackCountEnum.HiOptOne, new List<float>() { 0, 1, 1, 1, 1, 0, 0, 0, -1, 0 } },
+            { BlackjackCountEnum.HiOptTwo, new List<float>() {1, 1, 2, 2, 1, 1, 0,  0, -2, 0 } },
+            { BlackjackCountEnum.KO, new List<float>() {1, 1, 1, 1, 1, 1, 0,  0, -1, -1 } },
+            { BlackjackCountEnum.OmegaTwo, new List<float>() {1, 1, 2, 2, 2, 1, 0, -1, -2, 0 } },
+            { BlackjackCountEnum.RedSeven, new List<float>() {1, 1, 1, 1, 1, 0.5f, 0,  0, -1, -1 } },
+            { BlackjackCountEnum.ZenCount, new List<float>() {1, 1, 2, 2, 2, 1, 0,  0, -2, -1 } }
         };
         private readonly Dictionary<BlackjackCountEnum, float> countMap = new();
 
@@ -36,28 +36,41 @@ namespace Blackjack
         {
             shoe.Dealt += DealtHandler;
             shoe.Shuffling += ShuffledHandler;
+            foreach (BlackjackCountEnum system in Enum.GetValues(typeof(BlackjackCountEnum)))
+            {
+                countMap.Add(system, 0);
+                if (deltaMap[system].Count != 10)
+                {
+                    var i = 3;
+                }
+            }
         }
 
         public float this[BlackjackCountEnum system] => countMap[system];
 
-        private void DealtHandler(object obj, Shoe.DealtEventArgs args)
+        public void Reset()
+        {
+            foreach (BlackjackCountEnum system in countMap.Keys)
+            {
+                countMap[system] = 0;
+            }
+        }
+
+        private void DealtHandler(object _, Shoe.DealtEventArgs args)
         {
             foreach (Card card in args.DealtCards)
             {
                 int value = BlackjackHand.CardValue(card);
                 foreach (BlackjackCountEnum system in countMap.Keys)
                 {
-                    countMap[system] += deltaMap[system][value];
+                    countMap[system] += deltaMap[system][value - 2];
                 }
             }
         }
 
-        private void ShuffledHandler(object obj, EventArgs args)
+        private void ShuffledHandler(object obj, EventArgs _)
         {
-            foreach (BlackjackCountEnum system in countMap.Keys)
-            {
-                countMap[system] = 0;
-            }
+            Reset();
         }
     }
 }
---
+++ UnitTests/TestBlackjack/TestBlackjackCount.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cards;
+using Blackjack;
+
+namespace TestBlackjack
+{
+    [TestClass]
+    public class TestBlackjackCount
+    {
+        private Shoe shoe;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            shoe = new(1);
+        }
+
+        private void CheckAllZero(BlackjackCount count)
+        {
+            Array systems = Enum.GetValues(typeof(BlackjackCountEnum));
+            foreach (BlackjackCountEnum system in systems)
+            {
+                Assert.AreEqual(0, count[system]);
+            }
+        }
+
+        private void CheckAnyNonZero(BlackjackCount count)
+        {
+            Array systems = Enum.GetValues(typeof(BlackjackCountEnum));
+            IEnumerable<BlackjackCountEnum> systemsIter = systems.OfType<BlackjackCountEnum>();
+            int numNonZeroCounts = systemsIter.Select(s => count[s]).Where(c => c != 0).Count();
+
+            // this should probabilistically be true
+            Assert.IsTrue(numNonZeroCounts > 0);
+        }
+
+        [TestMethod]
+        public void TestCount()
+        {
+            BlackjackCount count = new(shoe);
+
+            CheckAllZero(count);
+
+            shoe.Deal(10);
+
+            CheckAnyNonZero(count);
+
+            shoe.Shuffle();
+
+            CheckAllZero(count);
+        }
+
+        [TestMethod]
+        public void TestReset()
+        {
+            BlackjackCount count = new(shoe);
+
+            CheckAllZero(count);
+
+            shoe.Deal(10);
+
+            CheckAnyNonZero(count);
+
+            count.Reset();
+
+            CheckAllZero(count);
+        }
+    }
+}
