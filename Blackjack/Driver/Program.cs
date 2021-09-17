b4e0f1348163c8bc591cf7efc4a3b64a937a6d9b
matthew <matthems@gmail.com>
2021-09-17 15:33:23 -0400

    option to play as bot

--- Blackjack/Driver/Program.cs
+++ Blackjack/Driver/Program.cs
@@ -8,14 +8,23 @@ namespace Driver
     {
         static void Main(string[] args)
         {
+            BlackjackPlayer player;
+
             BlackjackPlayer human = new("me");
             human.Bank.Deposit(1000);
+
+            BlackjackPlayer bot = new("bot");
+            bot.Bank.Deposit(1000);
 
             IBlackjackConfig config = new StandardBlackjackConfig();
 
             human.BettingPolicy = new MinimumBettingPolicy(config);
             human.DecisionPolicy = new InputDecisionPolicy();
             human.InsurancePolicy = new DeclineInsurancePolicy();
+
+            bot.BettingPolicy = new MinimumBettingPolicy(config);
+            bot.DecisionPolicy = new BasicStrategyPolicies();
+            bot.InsurancePolicy = new DeclineInsurancePolicy();
 
             BlackjackTable table = new(1, config);
             BlackjackEventLogger eventLogger = new();
@@ -30,11 +39,20 @@ namespace Driver
             human.InsurancePolicy.Insured += eventLogger.OnInsuranceDecision;
             human.Bank.Withdrawn += eventLogger.OnSpent;
             human.Bank.Deposited += eventLogger.OnEarned;
+
+            bot.BettingPolicy.Betting += eventLogger.OnBetMade;
+            bot.DecisionPolicy.Decided += eventLogger.OnDecisionMade;
+            bot.InsurancePolicy.Insured += eventLogger.OnInsuranceDecision;
+            bot.Bank.Withdrawn += eventLogger.OnSpent;
+            bot.Bank.Deposited += eventLogger.OnEarned;
+
             table.TableBank.Withdrawn += eventLogger.OnSpent;
             table.TableBank.Deposited += eventLogger.OnEarned;
 
-            table.SeatPlayer(human);
-            Console.WriteLine($"Welcome {human.Name}, good luck!");
+            //player = human;
+            player = bot;
+
+            table.SeatPlayer(player);
 
             int numRounds = 0;
             while(true)
@@ -42,11 +60,15 @@ namespace Driver
                 table.PlayRound();
                 numRounds++;
 
-                Console.WriteLine($"{numRounds} rounds played, you have ${human.Bank.Balance}");
-                Console.WriteLine("continue (y/n)? > ");
-                if (Console.ReadLine().ToLower().StartsWith("n"))
+                Console.WriteLine($"{numRounds} rounds played, you have ${player.Bank.Balance}");
+
+                if (player.DecisionPolicy is InputDecisionPolicy)
                 {
-                    break;
+                    Console.WriteLine("continue (y/n)? > ");
+                    if (Console.ReadLine().ToLower().StartsWith("n"))
+                    {
+                        break;
+                    }
                 }
             }
         }
