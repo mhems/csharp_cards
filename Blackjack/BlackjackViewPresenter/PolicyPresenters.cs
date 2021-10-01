a930e9a3c5cab88f4bc76c5b14bff31e275ec10a
matthew <matthems@gmail.com>
2021-10-01 17:38:24 -0400

    initial attempt at syncing up decision views to presenters

--- Blackjack/BlackjackViewPresenter/PolicyPresenters.cs
+++ Blackjack/BlackjackViewPresenter/PolicyPresenters.cs
@@ -2,68 +2,101 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Blackjack;
 using Cards;
 
 namespace BlackjackViewPresenter
 {
-    public class InteractiveDecisionPolicy : BlackjackDecisionPolicy
+    public interface IDecisionWaiter
+    {
+        public bool DecisionMade { get; set; }
+    }
+
+    public static class DecisionUtils
+    {
+        public static void WaitOnEvent(this IDecisionWaiter waiter)
+        {
+            // TODO: polling seems sub-optimal.
+            // there should be a library method to sync wait on an event to fire.
+            // i cannot find one though.
+            while (!waiter.DecisionMade)
+            {
+                Thread.Sleep(1);
+            }
+        }
+
+        public static EventHandler<EventArgs> GetDecisionHandler(this IDecisionWaiter waiter)
+        {
+            return new EventHandler<EventArgs>((sender, e) => waiter.DecisionMade = true);
+        }
+    }
+
+    public class InteractiveDecisionPolicy : BlackjackDecisionPolicy, IDecisionWaiter
     {
         private readonly IBlackjackDecisionView view;
+        public bool DecisionMade { get; set; }
         public InteractiveDecisionPolicy(IBlackjackDecisionView view)
         {
             this.view = view;
+            view.DecisionMade += this.GetDecisionHandler();
         }
         protected override BlackjackActionEnum DecideInner(BlackjackHand hand, Card upCard, HashSet<BlackjackActionEnum> availableActions)
         {
             view.Prompt(hand, upCard, availableActions);
-            // wait
+            this.WaitOnEvent();
             return view.Action;
         }
     }
 
-    public class InteractiveBettingPolicy : BlackjackBettingPolicy
+    public class InteractiveBettingPolicy : BlackjackBettingPolicy, IDecisionWaiter
     {
         private readonly IBlackjackBetView view;
+        public bool DecisionMade { get; set; }
         public InteractiveBettingPolicy(IBlackjackBetView view)
         {
             this.view = view;
+            view.BetMade += this.GetDecisionHandler();
         }
         protected override int BetInner(int minimumBet)
         {
             view.Prompt(minimumBet);
-            // wait
+            this.WaitOnEvent();
             return view.Bet;
         }
     }
 
-    public class InteractiveInsurancePolicy : BlackjackInsurancePolicy
+    public class InteractiveInsurancePolicy : BlackjackInsurancePolicy, IDecisionWaiter
     {
         private readonly IBlackjackInsuranceView view;
+        public bool DecisionMade { get; set; }
         public InteractiveInsurancePolicy(IBlackjackInsuranceView view)
         {
             this.view = view;
+            view.DecisionMade += this.GetDecisionHandler();
         }
         protected override bool InsureInner(BlackjackHand hand, Card upCard)
         {
             view.Prompt(hand, upCard);
-            // wait
+            this.WaitOnEvent();
             return view.Insured;
         }
     }
 
-    public class InteractiveSurrenderPolicy : BlackjackEarlySurrenderPolicy
+    public class InteractiveSurrenderPolicy : BlackjackEarlySurrenderPolicy, IDecisionWaiter
     {
         private readonly IBlackjackEarlySurrenderView view;
+        public bool DecisionMade { get; set; }
         public InteractiveSurrenderPolicy(IBlackjackEarlySurrenderView view)
         {
             this.view = view;
+            view.DecisionMade += this.GetDecisionHandler();
         }
         protected override bool SurrenderInner(BlackjackHand hand, Card upCard)
         {
             view.Prompt(hand, upCard);
-            // wait
+            this.WaitOnEvent();
             return view.Surrendered;
         }
     }
